5300853efda090dc4427fa36a7c6ef6afde7bb84
David <DavidjRay2022@gmail.com>
2023-03-01 05:34:45 -0600

    Added Validation annotations to Transfer model

--- tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -2,17 +2,20 @@ package com.techelevator.tenmo.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data  //uses lombok to set up getters, setters, and constructor
 public class Transfer {
 
-//TODO set up annotations
 private int id;
 private int transferTypeId;
 private int transferStatusId;
+@NotNull
 private int accountFrom;
+@NotNull
 private int accountTo;
+@NotNull
 private BigDecimal amount;
 
 
